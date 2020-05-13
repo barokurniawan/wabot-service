b806c5a4e000b502147ca5d86efbb86bc1df7510
barokurniawan <kurniawan9411n@gmail.com>
2020-05-13 07:30:59 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wabot-service
+# wabot-service
---
+++ app.js
@@ -0,0 +1,63 @@
+const { Client } = require('whatsapp-web.js');
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const SESSION_FILE_PATH = './session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({ session: sessionCfg });
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+    sessionCfg = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessfull
+    console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async msg => {
+    if (msg.body == '!ping') {
+        const chat = await msg.getChat();
+        chat.sendStateTyping();
+        client.sendMessage(msg.from, 'pong');
+        chat.clearState();
+    } else {
+        const chat = await msg.getChat();
+        chat.sendStateTyping();
+        client.sendMessage(msg.from, 'Perintah tidak diketahui');
+        chat.clearState();
+    }
+});
+
+client.initialize();
+
+//create a server object:
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true).query;
+    if (typeof q.phone != "undefined") {
+        client.sendMessage(q.phone + "@c.us", q.message);
+    }
+
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
