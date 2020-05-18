69594cfdf4359f2c1ac55079d6462059da1db7b7
barokurniawan <kurniawan9411n@gmail.com>
2020-05-18 20:23:28 +0700

    add docker support

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
 
-const client = new Client({ session: sessionCfg });
+const client = new Client({ puppeteer: { headless: true, args: ['--no-sandbox'] }, session: sessionCfg });
 
 client.on('authenticated', (session) => {
     console.log('AUTHENTICATED', session);
