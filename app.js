7eb0d92db0c8208cd04d887012c1c200584b51aa
barokurniawan <kurniawan9411n@gmail.com>
2020-05-23 09:24:27 +0700

    improve app.js

--- app.js
+++ app.js
@@ -15,13 +15,34 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
 
-const client = new Client({ puppeteer: { headless: true, args: ['--no-sandbox'] }, session: sessionCfg });
+let client = new Client({ puppeteer: { headless: true, args: ['--no-sandbox'] }, session: sessionCfg });
 const routeMain = new RouteMain();
 routeMain.setBotClient(client);
 routeMain.setQRModule(QRCode);
 
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+
+    if (fs.existsSync(SESSION_FILE_PATH)) {
+        // remove current session file
+        fs.unlinkSync(SESSION_FILE_PATH);
+        sessionCfg = null;
+    }
+
+    routeMain.isClientReady(false);
+
+    //destroy client instant 
+    client.destroy().then(function () {
+        console.log('Client is shutdown..');
+        //start whatsapp client engine 
+        client.initialize().then(function () {
+            console.log('Fresh copy is ready..');
+        });
+    });
+});
+
 client.on('authenticated', (session) => {
-    console.log('AUTHENTICATED', session);
+    console.log('login sucess..');
     sessionCfg = session;
     fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
         if (err) {
@@ -36,27 +57,28 @@ client.on('auth_failure', msg => {
 
     // remove current session file
     fs.unlinkSync(SESSION_FILE_PATH);
+    sessionCfg = null;
 
     //destroy client instant 
     client.destroy().then(function () {
-        /*
-            we are using docker with restart:always, exit the 
-            system to make container reboot, so we get a new fresh copy
-            remember, there is a downtime.
-        */
-        process.exit(1);
+        console.log('Client is shutdown..');
+        //start whatsapp client engine 
+        client.initialize().then(function () {
+            console.log('Fresh copy is ready..');
+        });
     });
 });
 
 client.on('qr', (qr) => {
     // Generate and scan this code with your phone
     routeMain.setQRCode(qr);
-    console.log("QR is ready");
+    console.log("qrcode is ready..");
 });
 
 client.on('ready', () => {
     routeMain.setQRCode(null);
-    console.log('Client is ready!');
+    routeMain.isClientReady(true);
+    console.log('Client is ready..');
 });
 
 client.on('message', async msg => {
@@ -72,22 +94,29 @@ client.on('message', async msg => {
 //define route request
 app.get('/', routeMain.handleIndex.bind(routeMain));
 app.get('/device', routeMain.handleDevice.bind(routeMain));
+app.post('/', routeMain.handleIndex.bind(routeMain));
+app.post('/device', routeMain.handleDevice.bind(routeMain));
 app.get('/qr/scan', routeMain.handleScanner.bind(routeMain));
 
 //reset will remove session so we need to scan new qrcode
 app.get('/api/reset', function (req, res) {
+    if (fs.existsSync(SESSION_FILE_PATH) == false || routeMain.clientReady == false) {
+        res.setHeader('Content-Type', 'Application/Json');
+        res.send(JSON.stringify({
+            info: false,
+            status: 'Reset a non ready client is illegal.'
+        }));
+
+        return;
+    }
+
+    console.log('Shuting down the client..');
     // remove current session file
     fs.unlinkSync(SESSION_FILE_PATH);
+    sessionCfg = null;
 
-    //destroy client instant 
-    client.destroy().then(function () {
-        /*
-            we are using docker with restart:always, exit the 
-            system to make container reboot, so we get a new fresh copy
-            remember, there is a downtime.
-        */
-        process.exit(1);
-    });
+    //the best solution for reseting client is reset the container it self
+    process.exit(1);
 });
 
 //cek server status, to check uptime after downtime
@@ -100,7 +129,9 @@ app.get('/api/health', function (req, res) {
 });
 
 //start whatsapp client engine 
-client.initialize();
+client.initialize().catch(function (err) {
+    console.error(err);
+});
 
 //start http server 
-app.listen(port, host, () => console.log(`listening at http://${host}:${port}`))
+app.listen(port, host, () => console.log(`listening at http://${host}:${port}`));
--- src/http/route-main.js
+++ src/http/route-main.js
@@ -1,4 +1,5 @@
 const { MessageMedia } = require('whatsapp-web.js');
+const { PassThrough } = require('stream');
 const axios = require('axios').default;
 
 const fileToBase64 = async function (file) {
@@ -12,8 +13,13 @@ module.exports = class RouteMain {
 
     constructor() {
         this.client = null;
+        this.clientReady = false;
         this._qrcode = null;
         this.QRModule = null;
+    }
+
+    isClientReady(status) {
+        this.clientReady = status;
     }
 
     setBotClient(botClient) {
@@ -41,6 +47,16 @@ module.exports = class RouteMain {
     }
 
     async handleIndex(req, res) {
+        var _that = this;
+        res.setHeader('Content-Type', 'Application/Json');
+        if (_that.clientReady == false) {
+            res.send(JSON.stringify({
+                info: false,
+                status: 'Client is not ready'
+            }));
+            return;
+        }
+
         if (typeof req.query.phone != "undefined") {
             if (typeof req.query.mime != "undefined" && typeof req.query.file != "undefined" && typeof req.query.filename != "undefined") {
                 const chat = await this.client.getChatById(req.query.phone + "@c.us");
@@ -64,7 +80,6 @@ module.exports = class RouteMain {
             }
         }
 
-        res.setHeader('Content-Type', 'Application/Json');
         res.send(JSON.stringify({
             info: true,
             status: 'send to queue',
@@ -96,29 +111,19 @@ module.exports = class RouteMain {
         });
     }
 
-    handleScanner(req, res) {
+    async handleScanner(req, res) {
         var _that = this;
         if (_that.getQRCode()) {
-            _that.getQRModule().toDataURL(_that.getQRCode()).then(function (imgdata) {
-                res.setHeader("Content-Type", "text/html");
-                res.status(200).write(`<html>
-                    <head>
-                        <title>Scan this qrcode</title>
-                        <meta name="viewport" content="width=device-width">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <style>
-                            body, html{
-                                margin: 0; padding: 0;
-                                width: 100%; height: 100%;
-                                text-align: center;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <img style="margin-top: 10%;" src="`+ imgdata + `">
-                    </body>
-                </html>`);
-            });
+            const qrStream = new PassThrough();
+            await _that.getQRModule().toFileStream(qrStream, _that.getQRCode(),
+                {
+                    type: 'png',
+                    width: 350,
+                    errorCorrectionLevel: 'H'
+                }
+            );
+
+            qrStream.pipe(res);
         } else {
             res.setHeader("Content-Type", "Application/Json");
             res.status(200).send(JSON.stringify({
