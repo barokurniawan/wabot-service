dece787dd121ac7d364ff94a078c559685ba3b39
barokurniawan <kurniawan9411n@gmail.com>
2020-05-29 00:42:38 +0700

    implement redis

--- interface/web/bootstrap/app.php
+++ interface/web/bootstrap/app.php
@@ -60,6 +60,7 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('database');
 
 /*
 |--------------------------------------------------------------------------
@@ -94,6 +95,8 @@ $app->configure('app');
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+
+$app->register(Illuminate\Redis\RedisServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
